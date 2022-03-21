fb88eefb3550c897e43f97f4c2ea08fc0cc736e2
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-03-21 15:16:12 -0500

    init

--- SpeedyURLs/Properties/AssemblyInfo.cs
+++ SpeedyURLs/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModNameGoesHere")]
+[assembly: AssemblyTitle("SpeedyURLs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModNameGoesHere")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("SpeedyURLs")]
+[assembly: AssemblyCopyright("Hiatus Labs ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ SpeedyURLs/SpeedyURLs.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using System;
+using BaseX;
+using FrooxEngine.UIX;
+
+namespace SpeedyURLNamespace
+{
+    public class SpeedyURLs : NeosMod
+    {
+        public override string Name => "SpeedyURLs";
+        public override string Author => "dfgHiatus";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/dfgHiatus/SpeedyURLs/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.dfgHiatus.SpeedyURLs");
+            harmony.PatchAll();
+        }
+		
+        [HarmonyPatch(typeof(HyperlinkOpenDialog), "Setup")]
+        public class HyperlinkOpenDialogPatch
+        {
+            public static bool Prefix(Hyperlink __instance, Uri uri, string reason, 
+                SyncRef<Text> ____hyperlinkText, SyncRef<Text> ____reasonText, SyncRef<Button> ____openButton)
+            {
+                __instance.URL.Value = uri;
+                ____hyperlinkText.Target.Content.Value = uri?.ToString();
+                if (reason == null)
+                    ____reasonText.Target.Content.SetLocalized("Security.HostAccess.NoReason");
+                else
+                    ____reasonText.Target.Content.SetLocalized("Security.HostAccess.Reason", null, nameof(reason), reason);
+                // Remove the 5-second delay on hyperlinks
+                // ____openButton.Target.EnableTimeout(0);
+                return false;
+            }
+        }
+    }
+}
