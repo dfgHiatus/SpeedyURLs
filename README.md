53eba74deffda029a55b470d14260e5e1b47832c
dfgHiatus <evansBM@protonmail.com>
2023-10-10 08:15:58 -0700

    Update for Resonite

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # SpeedyURLs
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that removes the 5-second cool-down on url consent popups.
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that removes the 5-second cool-down on url consent popups.
 
 ## Installation
-1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
-1. Place [SpeedyURLs.dll](https://github.com/dfgHiatus/SpeedyURLs/releases/download/v1.0.0/SpeedyURLs.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
-1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+1. Place [SpeedyURLs.dll](https://github.com/dfgHiatus/SpeedyURLs/releases/download/v1.0.0/SpeedyURLs.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
--- SpeedyURLs/SpeedyURLs.cs
+++ SpeedyURLs/SpeedyURLs.cs
@@ -1,31 +1,28 @@
-﻿using HarmonyLib;
-using NeosModLoader;
-using FrooxEngine;
-using System;
-using BaseX;
+﻿using FrooxEngine;
 using FrooxEngine.UIX;
+using HarmonyLib;
+using ResoniteModLoader;
+using System;
 
-namespace SpeedyURLNamespace
+namespace SpeedyURLNamespace;
+
+public class SpeedyURLs : ResoniteMod
 {
-    public class SpeedyURLs : NeosMod
+    public override string Name => "SpeedyURLs";
+    public override string Author => "dfgHiatus";
+    public override string Version => "2.0.0";
+    public override string Link => "https://github.com/dfgHiatus/SpeedyURLs/";
+    public override void OnEngineInit()
     {
-        public override string Name => "SpeedyURLs";
-        public override string Author => "dfgHiatus";
-        public override string Version => "1.1.0";
-        public override string Link => "https://github.com/dfgHiatus/SpeedyURLs/";
-        public override void OnEngineInit()
-        {
-            Harmony harmony = new Harmony("net.dfgHiatus.SpeedyURLs");
-            harmony.PatchAll();
-        }
+        new Harmony("net.dfgHiatus.SpeedyURLs").PatchAll();
+    }
 
-        [HarmonyPatch(typeof(UIExtensions), "EnableTimeout", new Type[] {typeof(Component), typeof(IField<bool>), typeof(IField<string>), typeof(int)})]
-        public class HyperlinkOpenDialogPatch
+    [HarmonyPatch(typeof(UIExtensions), "EnableTimeout", new Type[] {typeof(Component), typeof(IField<bool>), typeof(IField<string>), typeof(int)})]
+    public class HyperlinkOpenDialogPatch
+    {
+        public static void Prefix(ref int timeout)
         {
-            public static void Prefix(ref int timeout)
-            {
-                timeout = 0;
-            }
+            timeout = 0;
         }
     }
 }
